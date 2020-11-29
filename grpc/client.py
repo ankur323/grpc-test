6889c53a504208be9e42c354d294d9298d2443f4
Ankur Jaiswal <33278284+ankur323@users.noreply.github.com>
2020-11-29 18:46:00 +0530

    #0 sample project

---
+++ grpc/client.py
@@ -0,0 +1,14 @@
+import grpc
+import calculator_pb2
+import calculator_pb2_grpc
+
+channel = grpc.insecure_channel('localhost:50051')
+
+
+number = calculator_pb2.Number(value=100)
+
+stub = calculator_pb2_grpc.CalculatorStub(channel)
+
+response = stub.SquareRoot(number)
+
+print(response.value)
